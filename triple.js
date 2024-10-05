c98db641edc9e4aff902916e899bce3c2f38a6ed
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-10-06 03:49:54 +0600

    triple and double equal example

---
+++ triple.js
@@ -0,0 +1,14 @@
+// type coercion
+// type conversion
+// type casting
+
+// always use === triple equal
+// equal comparison does not work for non-primitive
+const first = [2, 4, 6];
+const second = [2, 4, 6];
+if (first === second) {
+    console.log('Values are equal');
+}
+else {
+    console.log("Values are not equal");
+}
