4f6712c374ee813476ea4397343e87875c9ded09
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-10-07 16:45:08 +0600

    hoisting example

---
+++ hoisting.js
@@ -0,0 +1,16 @@
+print5(5);
+print10(10);
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+// console.log('outside', i);
+
+function print5() {
+    console.log('inside print5', 5);
+}
+
+
+const print10 = function () {
+    console.log('inside print10', 10);
+}
